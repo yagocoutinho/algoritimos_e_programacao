ac51871bcf2d6a8006e51a62b9f01d8d566d3124
yagocoutinho <yago95563568@gmail.com>
2023-09-29 09:24:28 -0300

    aula

---
+++ 1180.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(){
+    int tamanhoVetor, valor;
+    scanf("%d", &tamanhoVetor);
+
+    int vetor[tamanhoVetor];
+    
+    //lê os valores e salva cada posicao do vetor
+    for(int posicao = 0; posicao< tamanhoVetor;posicao ++){
+    scanf("%d", &valor);
+    vetor[posicao] = valor;
+    }
+
+
+    // encontra o menor valor
+    int menorValor = vetor[0];
+    int posicaoMenorValor = 0;
+    for(int posicao = 1;posicao < tamanhoVetor;posicao++){
+        if (vetor[posicao] < menorValor){
+        menorValor = vetor[posicao];
+        posicaoMenorValor = posicao;
+       }
+    }
+
+    printf("Menor valor: %d\n", menorValor);
+    printf("Posicao: %d\n", posicaoMenorValor);
+
+    return 0;
+}
